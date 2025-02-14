2eac05a33c8c61a504269879997d83c680e9f0b1
MEGA <MEGA@gmail.com>
2025-02-14 17:11:24 +0200

    Add project files.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    using System;
+
+    class Program
+    {
+        static bool IsPowerOfTwo(int n)
+        {
+            
+            if (n == 1)
+                return true;
+
+            if (n < 1)
+                return false;
+            
+            if (n % 2 != 0)
+                return false; 
+
+            return IsPowerOfTwo(n / 2); 
+        }
+        static void Main()
+        {
+            
+        }
+    }
+}
---
+++ zad 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_3
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int num = 1234;
+            Console.WriteLine(reverse(num));
+        }
+
+        static int reverse(int n)
+        {
+
+            if (n == 0)
+            {
+                return 0;
+            }
+
+
+            return (n % 10) * (int)Math.Pow(10, (int)Math.Log10(n)) + reverse(n / 10);
+        }
+    }
+}
+
