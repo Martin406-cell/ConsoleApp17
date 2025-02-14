8de19dcf38e4450c29556b2317d159f843231ab7
MEGA <MEGA@gmail.com>
2025-02-14 17:15:57 +0200

    zad 4

---
+++ zad 4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_4
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int result = Power(2, 3);
+            Console.WriteLine(result);  
+        }
+
+        static int Power(int a, int b)
+        {
+
+            if (b == 0)
+            {
+                return 1;
+            }
+            else
+            {
+
+                return a * Power(a, b - 1);
+            }
+        }
+    }
+}
+
